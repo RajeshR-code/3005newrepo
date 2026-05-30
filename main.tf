428c666be0ffb732cfd2c044d5df80098298c6d7
Ubuntu <ubuntu@ip-172-31-35-220.ap-south-1.compute.internal>
2026-05-30 12:11:32 +0000

    maincofig

--- main.tf
+++ main.tf
@@ -5,17 +5,17 @@ resource "azurerm_resource_group" "myrg" {
 }
 
 resource "azurerm_virtual_network" "myvnet" {
-    name = "vnet1"
-    resource_group_name = "rr-rg"
-    location = "westeurope"
+    name = "r-vnet"
+    resource_group_name = azurerm_resource_group.myrg.name
+    location = azurerm_resource_group.myrg.location
     address_space = ["10.0.0.0/16"]
  
 }
 
 resource "azurerm_subnet" "mysubnet" {
     name = "vm-submet"
-    resource_group_name = "rr-rg"
-    virtual_network_name = "vnet1"
+    resource_group_name = azurerm_resource_group.myrg.name
+    virtual_network_name = azurerm_virtual_network.myvnet.name
     address_prefixes = ["10.0.1.0/24"]
   
 }
